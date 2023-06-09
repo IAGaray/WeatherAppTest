467f5797ee824b65cc5016dc9c7a4bd6c30ebef0
Ignacio <igarayvergara@gmail.com>
2023-06-09 05:04:39 -0400

    Se termina la app MV mejorando el css

--- custom.js
+++ custom.js
@@ -1,56 +1,79 @@
-const result = document.querySelector('result');
+const result = document.querySelector('.result'); // Agrega el símbolo "#" para seleccionar por id
 const form = document.querySelector('.get-weather');
-const nameCity= document.querySelector('#city');
-const nameCountry= document.querySelector('#Country');
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#Country');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (nameCity.value === '' || nameCountry.value === '') {
+    showError('Both fields are required');
+    return;
+  }
+
+  callAPI(nameCity.value, nameCountry.value);
+});
 
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault(); 
-// cuando se usa este metodo se previene que se cargue el form al seleccionar algo
-    
-    if(nameCity.value === '' || nameCountry.value === ''){
-        showError('Both fields are required')
-        return;
-    }
+function callAPI(city, country) {
+  const apiId = 'ingresa tu api aqui';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
 
-    callAPI(nameCity.value, nameCountry.value)
-    // console.log(nameCity.value)
-    // console.log(nameCountry.value)
-})
-function callAPI(city, country){
-    const apiId= 'Ingresa tu API AQUI!!!'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`
-    //https://api.openweathermap.org/data/2.5/weather?q=santiago,Chile&appid=API
-fetch(url)
-.then(data =>{
-    return (data.json()
-    )
-})
-.then(dataJSON => {
-    if(dataJSON.cod === '404'){
-        showError('City not found')
-    }else{
-        showError(dataJSON)
-    }
-    })
-}
-function showWeather(data){
-    const{
+  clearHTML()
 
-    } = data
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    showWeather(data);
+  })
+  .catch(error => {
+    console.error('Error fetching weather data:', error);
+  });
 }
 
+function showWeather(data) {
+  const { name, main: { temp, temp_max, temp_min }, weather: [arr] } = data;
 
+  const degrees = kelvinToCentigrade(temp);
+  const min = kelvinToCentigrade(temp_min);
+  const max = kelvinToCentigrade(temp_max);
 
+  const content = document.createElement('div');
+  content.innerHTML =
+    `
+    <h1>Selec your countre and city</h1>
 
-function showError(message){
-    console.log(message)
-const alert = document.createElement('p')
-alert.classList.add('alert-message')
-alert.innerHTML = message;
+    <h5>The weather in ${name}</h5>
+    <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="">
+    <h2>${degrees}°C</h2>
+    <p>Max: ${max}°C</p>
+    <p>Min: ${min}°C</p>`;
 
-form.appendChild(alert)
-setTimeout(() =>{
-    alert.remove()
-}, 3000)
+  result.appendChild(content);
+
+  console.log(name);
+  console.log(temp);
+  console.log(temp_max);
+  console.log(temp_min);
+  console.log(arr.icon);
+}
+
+function showError(message) {
+  console.log(message);
+  const alert = document.createElement('p');
+  alert.classList.add('alert-message');
+  alert.innerHTML = message;
+
+  form.appendChild(alert);
+  setTimeout(() => {
+    alert.remove();
+  }, 3000);
+}
+
+function kelvinToCentigrade(temp) {
+  return parseInt(temp - 273.15);
+}
+
+function clearHTML() {
+  result.innerHTML = '';
 }
